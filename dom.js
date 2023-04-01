dc5064c8107d4878813494297f8e6cc8bf4faeb0
tejas <sadadetej.27@gmail.com>
2023-04-01 22:24:06 +0530

    tag_query_updation

--- dom.js
+++ dom.js
@@ -17,19 +17,41 @@ console.log(headerTitle);
 //headerTitle.style.borderBottom='solid 3px #000';
 header.style.borderBottom='solid 3px #000';  //now here we apply border to its
 
-//GET_ELEMENT_BY_CLASS_NAME
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2';
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='tomato';
+// //GET_ELEMENT_BY_CLASS_NAME
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='tomato';
 
-//gives error
-//items[1].style.backgroundColor='#f4f4f4';
+// //gives error
+// //items[1].style.backgroundColor='#f4f4f4';
 
-for(var i=0;i<items.length;i++){
-    items[i].textContent='Item '+i;
-    items[i].style.backgroundColor='#f4f4f4';
-    items[i].style.fontWeight='bold';
+// for(var i=0;i<items.length;i++){
+//     items[i].textContent='Item '+i;
+//     items[i].style.backgroundColor='#f4f4f4';
+//     items[i].style.fontWeight='bold';
+// }
+
+//GET_ELEMENT_BY_TAG_NAME
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='tomato';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+    li[i].textContent='Item '+i;
+    li[i].style.backgroundColor='tomato';
 }
+
+
+// QUERYSELECTOR
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
+
+var input=document.querySelector('input');
+input.value='Hello World'
