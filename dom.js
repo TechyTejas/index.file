f527643f85677793f62a2e557de5d4f73f752ac0
tejas <sadadetej.27@gmail.com>
2023-04-01 20:04:36 +0530

    class_changes

--- dom.js
+++ dom.js
@@ -16,3 +16,20 @@ console.log(headerTitle);
 //headerTitle.innerHTML='<h3>Hello</h3>';  // we put html code into html code bu using this
 //headerTitle.style.borderBottom='solid 3px #000';
 header.style.borderBottom='solid 3px #000';  //now here we apply border to its
+
+//GET_ELEMENT_BY_CLASS_NAME
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='tomato';
+
+//gives error
+//items[1].style.backgroundColor='#f4f4f4';
+
+for(var i=0;i<items.length;i++){
+    items[i].textContent='Item '+i;
+    items[i].style.backgroundColor='#f4f4f4';
+    items[i].style.fontWeight='bold';
+}
