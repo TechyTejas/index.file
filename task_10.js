2e23ea9b0e3eaa2ed94da22378362a5f0f8df308
tejas <sadadetej.27@gmail.com>
2023-04-03 16:45:18 +0530

    task_10

---
+++ task_10.js
@@ -0,0 +1,14 @@
+prnt.addEventListener('submit', onsubmit);
+function onsubmit(e){
+  e.preventDefault();
+  let name =document.getElementById('name').value;
+  console.log(name);
+  let email =document.getElementById('exampleInputEmail1').value;
+  console.log(email);
+  let number =document.getElementById('number').value;
+  console.log(number);
+  
+  let obj={name,email,number};
+  localStorage.setItem(email,obj)
+}
+
